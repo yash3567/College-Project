ccb53fc1b6cb698ba6efcdd0ecb16aca2a9549af
yash3567 <yashnandeshwar32@gmail.com>
2025-03-16 00:45:08 +0530

    update

--- backend/Server.js
+++ backend/Server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-import path from "path";
+// import path from "path";
 import { v2 as cloudinary } from "cloudinary";
 import connectMongoDB from "./db/connectMongoDB.js";
 import cors from "cors"
@@ -22,7 +22,7 @@ cloudinary.config({
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-const __dirname = path.resolve();
+// const __dirname = path.resolve();
 
 app.use(express.json({ limit: "5mb" }));
 app.use(express.urlencoded({ extended: true }));
@@ -39,12 +39,12 @@ app.use("/api", projectRoutes);
 app.use("/api", emailVerifyRoutes);
 
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/dist")));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-  });
-}
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "/frontend/dist")));
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+//   });
+// }
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
