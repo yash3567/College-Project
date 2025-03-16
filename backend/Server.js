280f5a2411ce0ff55f5c028849dbd59278e63fa4
yash3567 <yashnandeshwar32@gmail.com>
2025-03-16 18:23:44 +0530

    code changes

--- backend/Server.js
+++ backend/Server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-// import path from "path";
+import path from "path";
 import { v2 as cloudinary } from "cloudinary";
 import connectMongoDB from "./db/connectMongoDB.js";
 import cors from "cors"
@@ -22,7 +22,7 @@ cloudinary.config({
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-// const __dirname = path.resolve();
+const __dirname = path.resolve();
 
 app.use(express.json({ limit: "5mb" }));
 app.use(express.urlencoded({ extended: true }));
