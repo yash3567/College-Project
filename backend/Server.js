0b962819dd0cd33491cf57dfc62fde6c46eeaf9d
yash3567 <yashnandeshwar32@gmail.com>
2025-03-16 17:52:49 +0530

    old code updated

--- backend/Server.js
+++ backend/Server.js
@@ -39,12 +39,12 @@ app.use("/api", projectRoutes);
 app.use("/api", emailVerifyRoutes);
 
 
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static(path.join(__dirname, "/frontend/dist")));
-//   app.get("*", (req, res) => {
-//     res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-//   });
-// }
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
