60a85d878f0e72a676e272a7a39fe0d07e5f427f
yash3567 <yashnandeshwar32@gmail.com>
2025-03-17 21:53:33 +0530

    remove: node_modules from commit
    Signed-off-by: yash3567 <yashnandeshwar32@gmail.com>

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,6 +1,6 @@
 import { generateTokenAndSetCookie } from "../lib/utils/generateToken.js";
 import User from "../models/user.model.js";
-import bcrypt from "bcryptjs";
+import bcrypt from "bcrypt";
 
 export const signup = async (req, res) => {
     try {
--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,4 +1,4 @@
-import bcrypt from "bcryptjs";
+import bcrypt from "bcrypt";
 import { v2 as cloudinary } from "cloudinary";
 
 // models
